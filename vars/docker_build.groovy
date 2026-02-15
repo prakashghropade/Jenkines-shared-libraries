f96c9a3556191f5fb807a560498134f519a80670
Prakash Ghorpade <132913023+prakashghropade@users.noreply.github.com>
2026-02-15 17:26:25 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,8 @@
 def call(String ProjectName, String ImageTag, String  DockerHubuser){
+
+   echo 'This is building the image'
+  
   sh "docker build -t ${DockerHubuser}/${ProjectName}:${ImageTag} ."
+
+     echo 'Docker image build completed successfully'
 }
